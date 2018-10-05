00aac249e3c6e36bde87875100594101a45ceea2
Paul Fairless <paul.fairless@gmail.com>
2018-10-05 14:11:35 +0100

    added helper

---
+++ src/com/endource/CodeDeployHelper.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+package com.endource
+
+def runBuild(name, specFile, artifactDir) {
+    script {
+        def buildSpec = readFile(specFile)
+        def result = awsCodeBuild(
+            projectName: name,
+            awsAccessKey: "${env.AWS_ACCESS_KEY_ID}", awsSecretKey: "${env.AWS_SECRET_ACCESS_KEY}", credentialsType: 'keys',region: "${env.AWS_DEFAULT_REGION}",
+            artifactLocationOverride: 'endource-codebuild', artifactNameOverride: 'reports.zip', artifactNamespaceOverride: 'BUILD_ID', artifactPackagingOverride: 'ZIP', artifactPathOverride: name, artifactTypeOverride: 'S3',
+            buildSpecFile: buildSpec,
+            sourceControlType: 'project', sourceVersion: "${GIT_COMMIT}", sseAlgorithm: 'AES256')
+
+        sh "aws s3 cp s3://${result.getArtifactsLocation().split(':::')[1]} ${artifactDir}/reports.zip || true"
+        sh "unzip -d ${artifactDir} ${artifactDir}/reports.zip"
+    }
+}
