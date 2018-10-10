ce0a28bebc492d9936fbaecb6590fb464f212c65
Paul Fairless <paul.fairless@gmail.com>
2018-10-10 17:31:15 +0100

    still archive test results even on failure

--- src/com/endource/CodeBuildHelper.groovy
+++ src/com/endource/CodeBuildHelper.groovy
@@ -4,16 +4,23 @@ package com.endource
 def runBuild(name, specFile, artifactDir, envParams) {
     script {
         def buildSpec = readFile(specFile)
-        def result = awsCodeBuild(
-            projectName: name,
-            awsAccessKey: "${env.AWS_ACCESS_KEY_ID}", awsSecretKey: "${env.AWS_SECRET_ACCESS_KEY}", credentialsType: 'keys',region: "${env.AWS_DEFAULT_REGION}",
-            artifactLocationOverride: 'endource-codebuild', artifactNameOverride: 'reports.zip', artifactNamespaceOverride: 'BUILD_ID', artifactPackagingOverride: 'ZIP', artifactPathOverride: name, artifactTypeOverride: 'S3',
-            buildSpecFile: buildSpec,
-            envVariables: envParams,
-            sourceControlType: 'project', sourceVersion: "${GIT_COMMIT}", sseAlgorithm: 'AES256')
+        def result = null
+        try {
+            awsCodeBuild(
+                projectName: name,
+                awsAccessKey: "${env.AWS_ACCESS_KEY_ID}", awsSecretKey: "${env.AWS_SECRET_ACCESS_KEY}", credentialsType: 'keys',region: "${env.AWS_DEFAULT_REGION}",
+                artifactLocationOverride: 'endource-codebuild', artifactNameOverride: 'reports.zip', artifactNamespaceOverride: 'BUILD_ID', artifactPackagingOverride: 'ZIP', artifactPathOverride: name, artifactTypeOverride: 'S3',
+                buildSpecFile: buildSpec,
+                envVariables: envParams,
+                sourceControlType: 'project', sourceVersion: "${GIT_COMMIT}", sseAlgorithm: 'AES256')
+        } catch (Exception e) {
+            currentBuild.result = 'WARNING'
+        } finally {
+            sh "aws s3 cp s3://${result.getArtifactsLocation().split(':::')[1]} ${artifactDir}/reports.zip || true"
+            sh "unzip -d ${artifactDir} ${artifactDir}/reports.zip"
+        }
 
-        sh "aws s3 cp s3://${result.getArtifactsLocation().split(':::')[1]} ${artifactDir}/reports.zip || true"
-        sh "unzip -d ${artifactDir} ${artifactDir}/reports.zip"
+
     }
 }
 
