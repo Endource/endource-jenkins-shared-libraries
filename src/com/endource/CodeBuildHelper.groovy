1c60e661f30f1ee55a0b95a4f5f345abd3ea4c27
Paul Fairless <paul.fairless@gmail.com>
2018-10-10 11:26:28 +0100

    add env parameters

--- src/com/endource/CodeBuildHelper.groovy
+++ src/com/endource/CodeBuildHelper.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 package com.endource
 
-def runBuild(name, specFile, artifactDir) {
+def runBuild(name, specFile, artifactDir, envParams='') {
     script {
         def buildSpec = readFile(specFile)
         def result = awsCodeBuild(
@@ -9,6 +9,7 @@ def runBuild(name, specFile, artifactDir) {
             awsAccessKey: "${env.AWS_ACCESS_KEY_ID}", awsSecretKey: "${env.AWS_SECRET_ACCESS_KEY}", credentialsType: 'keys',region: "${env.AWS_DEFAULT_REGION}",
             artifactLocationOverride: 'endource-codebuild', artifactNameOverride: 'reports.zip', artifactNamespaceOverride: 'BUILD_ID', artifactPackagingOverride: 'ZIP', artifactPathOverride: name, artifactTypeOverride: 'S3',
             buildSpecFile: buildSpec,
+            envVariables: envParams,
             sourceControlType: 'project', sourceVersion: "${GIT_COMMIT}", sseAlgorithm: 'AES256')
 
         sh "aws s3 cp s3://${result.getArtifactsLocation().split(':::')[1]} ${artifactDir}/reports.zip || true"
