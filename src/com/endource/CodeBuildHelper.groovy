de7dd22050e70daad4a1622367b9b817201e2a2a
Paul Fairless <paul.fairless@gmail.com>
2018-10-11 09:21:58 +0100

    get artifact even on failure

--- src/com/endource/CodeBuildHelper.groovy
+++ src/com/endource/CodeBuildHelper.groovy
@@ -14,7 +14,8 @@ def runBuild(name, specFile, artifactDir, envParams) {
                 envVariables: envParams,
                 sourceControlType: 'project', sourceVersion: "${GIT_COMMIT}", sseAlgorithm: 'AES256')
         } catch (Exception e) {
-            currentBuild.result = 'WARNING'
+            result=e.getCodeBuildResult()
+            throw e
         } finally {
             sh "aws s3 cp s3://${result.getArtifactsLocation().split(':::')[1]} ${artifactDir}/reports.zip || true"
             sh "unzip -d ${artifactDir} ${artifactDir}/reports.zip"
